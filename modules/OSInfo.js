f1e76d51fd8297a55ba5bbae1b31e38e194dcd20
Halina <gella93@gmail.com>
2017-10-10 12:17:43 +0200

    adding and updating the files

--- modules/OSInfo.js
+++ modules/OSInfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var colors = require('colors');
 var uptime = require('./time_format');
 
 function getOSinfo() {
@@ -10,14 +11,13 @@ function getOSinfo() {
 	}
 	var release = os.release();
 	var cpu = os.cpus()[0].model;
-	/*var uptime = os.uptime();*/
 	var userInfo = os.userInfo();
-	console.log('System:' , type);
-	console.log('Release:' , release);
-	console.log('CPU model:' , cpu);        
-	console.log('Uptime:' , uptime.getFormattedUptime());
-	console.log('User name:' , userInfo.username);
-	console.log('Home dir:' , userInfo.homedir);
+	console.log('System:'.gray , type);
+	console.log('Release:'.red , release);
+	console.log('CPU model:'.blue , cpu);        
+	console.log('Uptime:'.green , uptime.getFormattedUptime());
+	console.log('User name:'.yellow , userInfo.username);
+	console.log('Home dir:'.rainbow , userInfo.homedir);
 }
 
 exports.print = getOSinfo;
